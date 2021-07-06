f537dde693ea6a49dc393be273d656e23fa921df
SidewalksAndSkeletons <SidewalksAndSkeletons@protonmail.com>
2021-07-06 23:46:45 +0500

    (+) Точка входа в программу

---
+++ Framework/Source/EntryPoint.cpp
@@ -0,0 +1,29 @@
+// *** Коды завершения программы
+enum class EXIT_TYPE : int
+{
+	// Успешно
+	SUCCESS = 0,
+
+	// Ошибка отладчика
+	DEBUG_ERROR,
+
+	// Ошибка инициализации
+	INIT_ERROR,
+
+	// Ошибка во время выполнения
+	RUNTIME_ERROR,
+
+	// Неизвестная ошибка
+	UNDEFINED_ERROR
+};
+
+// *** Точка входа в программу
+int main(int keysCount, char** sourceKeys)
+{
+	auto GetErrorCode = [](EXIT_TYPE ERROR_CODE) -> int
+	{
+		return static_cast<int>(ERROR_CODE);
+	};
+
+	return GetErrorCode(EXIT_TYPE::SUCCESS);
+}
---
+++ Framework/Source/Kernel.h
@@ -0,0 +1,5 @@
+#pragma once
+
+class CKernel final
+{
+};
